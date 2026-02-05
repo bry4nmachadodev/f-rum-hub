3ee9fcc3e9a417e086c5cfd8abc498d8f0ebca63
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-02-05 14:59:23 -0300

    feat: cria bean para authenticationManager

--- src/main/java/br/com/forum_hub/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/br/com/forum_hub/infra/security/ConfiguracoesSeguranca.java
@@ -1,28 +1,35 @@
-package br.com.forum_hub.infra.security;
+    package br.com.forum_hub.infra.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.http.SessionCreationPolicy;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class ConfiguracoesSeguranca {
+    @Configuration
+    @EnableWebSecurity
+    public class ConfiguracoesSeguranca {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(csrf -> csrf.disable())
-                .build();
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            return http
+                    .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .csrf(csrf -> csrf.disable())
+                    .build();
+        }
+
+        @Bean
+        public PasswordEncoder encriptador() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+            return authenticationConfiguration.getAuthenticationManager();
+        }
     }
-
-    @Bean
-    public PasswordEncoder encriptador() {
-        return new BCryptPasswordEncoder();
-    }
-}
