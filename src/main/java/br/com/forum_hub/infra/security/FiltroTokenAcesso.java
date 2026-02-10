10a181a6e00c7509ebfc657b4fe6cbdf9cf9fb89
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-02-10 14:43:08 -0300

    feat: verifica integridade do token

--- src/main/java/br/com/forum_hub/infra/security/FiltroTokenAcesso.java
+++ src/main/java/br/com/forum_hub/infra/security/FiltroTokenAcesso.java
@@ -1,9 +1,15 @@
 package br.com.forum_hub.infra.security;
 
+import br.com.forum_hub.domain.autenticacao.TokenService;
+import br.com.forum_hub.domain.usuario.Usuario;
+import br.com.forum_hub.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,18 +18,43 @@ import java.io.IOException;
 @Component
 public class FiltroTokenAcesso extends OncePerRequestFilter {
 
+    private final TokenService tokenService;
+    private final UsuarioRepository usuarioRepository;
+
+    public FiltroTokenAcesso(TokenService tokenService, UsuarioRepository usuarioRepository) {
+        this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
             //recuperar o token do USUÁRIO (requisição)
-            String token;
+            //adendo: vai remover o bearer deixando so o token
+            String token = recuperarTokenRequisicao(request);
 
             //verificar se o token é null
             if (token != null){
                 //validação do token
+                String email = tokenService.verificarToken(token);
+                Usuario usuario = usuarioRepository.findByEmailIgnoreCase(email).orElseThrow();
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
 
             filterChain.doFilter(request,response);
 
+    }
+
+
+    //pega o token e remove o BEARER
+    private String recuperarTokenRequisicao(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+           return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
     }
 
 }
