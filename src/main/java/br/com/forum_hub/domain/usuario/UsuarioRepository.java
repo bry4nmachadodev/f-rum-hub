bd97dba274087e6578b166e6afa3b184b46a6d75
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-02-18 17:52:19 -0300

    feat: cria método para encontrar byToken

--- src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmailIgnoreCaseAndVerificadoTrue(String email);
 
+    Optional<Usuario> findByToken(String codigo);
+
     /// repository - token - OPACO
     Optional<Usuario> findByRefreshToken(String refreshToken);
 }
