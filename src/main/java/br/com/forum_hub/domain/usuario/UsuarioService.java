e1986e98a9fb0d6165de1e1465a39aea81404880
bry4nmachadodev <bryanmachado1703@icloud.com>
2026-02-23 21:36:20 -0300

    feat: cria métodos alterar senha e editar perfil (USUARIO SERVICE)

--- src/main/java/br/com/forum_hub/domain/usuario/UsuarioService.java
+++ src/main/java/br/com/forum_hub/domain/usuario/UsuarioService.java
@@ -1,6 +1,7 @@
 package br.com.forum_hub.domain.usuario;
 
 import br.com.forum_hub.infra.email.EmailService;
+import br.com.forum_hub.infra.exception.RegraDeNegocioException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,13 +15,13 @@ public class UsuarioService implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
 
-    private final PasswordEncoder encriptador;
+    private final PasswordEncoder passwordEncoder;
 
     private final EmailService emailService;
 
-    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder encriptador, EmailService emailService) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder, EmailService emailService) {
         this.usuarioRepository = usuarioRepository;
-        this.encriptador = encriptador;
+        this.passwordEncoder = passwordEncoder;
         this.emailService = emailService;
     }
 
@@ -33,7 +34,7 @@ public class UsuarioService implements UserDetailsService {
 
     ///  cadastra no db - criptografa a senha antes de salvar
     public Usuario cadastrar(@Valid DadosCadastroUsuario dados) {
-        String senhaCriptografada = encriptador.encode(dados.senha());
+        String senhaCriptografada = passwordEncoder.encode(dados.senha());
         var usuario  = new Usuario(dados, senhaCriptografada);
 
         emailService.enviarEmailVerificacao(usuario);
@@ -45,5 +46,34 @@ public class UsuarioService implements UserDetailsService {
     public void verificarEmail(String codigo) {
         var usuario = usuarioRepository.findByToken(codigo).orElseThrow();
         usuario.verificar();
+    }
+
+    public Usuario buscarPeloNomeUsuario(String nomeUsuario) {
+        return usuarioRepository.findByNomeUsuarioIgnoreCaseAndVerificadoTrueAndAtivoTrue(nomeUsuario).orElseThrow(
+                () -> new RegraDeNegocioException("Usuário não encontrado!"));
+    }
+
+    @Transactional
+    public Usuario editarPerfil(Usuario usuario, DadosEdicaoUsuario dados) {
+        return usuario.alterarDados(dados);
+    }
+
+    @Transactional
+    public void alterarSenha(DadosAlteracaoSenha dados, Usuario logado) {
+        if(!passwordEncoder.matches(dados.senhaAtual(), logado.getPassword())){
+            throw new RegraDeNegocioException("Senha digitada não confere com senha atual!");
+        }
+
+        if(!dados.novaSenha().equals(dados.novaSenhaConfirmacao())){
+            throw new RegraDeNegocioException("Senha e confirmação não conferem!");
+        }
+
+        String senhaCriptografada = passwordEncoder.encode(dados.novaSenha());
+        logado.alterarSenha(senhaCriptografada);
+    }
+
+    @Transactional
+    public void desativarUsuario(Usuario usuario) {
+        usuario.desativar();
     }
 }
